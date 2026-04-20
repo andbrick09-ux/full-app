6b21de2a078ef8625cf7398088b5b24671f00ba7
andbrick09-ux <andbrick09@gmail.com>
2026-04-19 21:24:24 -0400

    Add training suite (5 modules), update public-game branding, fix sw.js cache, standardize back links

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sirender-cache-v3';
+const CACHE_NAME = 'sirender-cache-v4';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,7 +9,32 @@ const urlsToCache = [
   './public-game.html',
   './styles.css',
   './manifest.json',
-  './intro.mp4'
+  './intro.mp4',
+  './favicon.svg',
+  './icon-512.png',
+  // Training
+  './training-menu.html',
+  './deep-throat-training.html',
+  './kegel-training.html',
+  './the-5-training.html',
+  './anal-plug-training.html',
+  './position-training.html',
+  // Checklists
+  './bdsm-checklist.html',
+  './sub-bdsm-checklist.html',
+  // Position reference images
+  './positions/pos_1_wait.png',
+  './positions/pos_2_attention.png',
+  './positions/pos_3_wall.png',
+  './positions/pos_4_service.png',
+  './positions/pos_5_nadu.png',
+  './positions/pos_6_collar.png',
+  './positions/pos_7_modest.png',
+  './positions/pos_8_tower.png',
+  './positions/pos_9_humble.png',
+  './positions/pos_10_table.png',
+  './positions/pos_11_please.png',
+  './positions/pos_12_inspection.png'
 ];
 
 self.addEventListener('install', event => {
